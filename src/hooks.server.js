059441a5d622162252d187795e7e53c5fe85c004
Kevin <dev@kevinb.run>
2023-09-23 09:24:23 +0200

    gestion hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,20 +4,22 @@ import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import {redirect} from "@sveltejs/kit";
 
 export const handle = async ({ event, resolve }) => {
-    event.locals.supabase = createSupabaseServerClient({
-        supabaseUrl: PUBLIC_SUPABASE_URL,
-        supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-        event,
-    })
 
     /**
      * a little helper that is written for convenience so that instead
      * of calling `const { data: { session } } = await supabase.auth.getSession()`
      * you just call this `await getSession()`
      */
-
+    if (event.url.pathname.startsWith('/admin')){
+        event.locals.supabase = createSupabaseServerClient({
+            supabaseUrl: PUBLIC_SUPABASE_URL,
+            supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+            event,
+        })
+    }
 
     if(event.url.pathname.startsWith('/admin/dashboard')){
+
         event.locals.getSession = async () => {
             const {
                 data: { session, error },
